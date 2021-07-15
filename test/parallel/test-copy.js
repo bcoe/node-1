448d77e787b6b410ba232efb6a949e0fd451ad61
bcoe <bencoe@google.com>
2021-07-15 08:23:42 -0700

    increase coverage fix bugs

--- test/parallel/test-copy.js
+++ test/parallel/test-copy.js
@@ -14,6 +14,7 @@ const {
   readdirSync,
   symlinkSync,
   statSync,
+  writeFileSync,
 } = fs;
 const net = require('net');
 const { dirname, join } = require('path');
@@ -27,7 +28,7 @@ function nextdir() {
   return join(tmpdir.path, `copy_${++dirc}`);
 }
 
-// Async version of copy.
+// Callback implementation of copy.
 
 // It copies a nested folder structure with files, folders, symlinks.
 {
@@ -39,25 +40,44 @@ function nextdir() {
   }));
 }
 
-// It throws error if existing symlink in dest is in subdirectory src.
-// TODO(bcoe): this behavior seemed strange to me, ask about it in review.
-//{
-//  const src = dirname(require.resolve('../fixtures/copy/kitchen-sink'));
-//  const dest = nextdir();
-//  copySync(src, dest);
-//  copy(src, dest, common.mustCall((err) => {
-//    assert.strictEqual(err.code, 'ERR_FS_COPY_TO_SUBDIRECTORY');
-//    assertDirEquivalent(src, dest);
-//  }));
-// }
+// It does not throw errors when directory is copied over.
+{
+  const src = dirname(require.resolve('../fixtures/copy/kitchen-sink'));
+  const dest = nextdir();
+  copySync(src, dest);
+  const initialStat = lstatSync(join(dest, 'README.md'));
+  copy(src, dest, common.mustCall((err) => {
+    assertDirEquivalent(src, dest);
+    // File should not have been copied over, so access times will be identical:
+    const finalStat = lstatSync(join(dest, 'README.md'));
+    assert.strictEqual(finalStat.ctime.getTime(), initialStat.ctime.getTime());
+  }));
+}
+
+// It overwrites existing files if force is true.
+{
+  const src = dirname(require.resolve('../fixtures/copy/kitchen-sink'));
+  const dest = nextdir();
+  copySync(src, dest);
+  const initialStat = lstatSync(join(dest, 'README.md'));
+  copy(src, dest, { force: true }, common.mustCall((err) => {
+    assertDirEquivalent(src, dest);
+    // File was copied over, so creation time will differ.
+    const finalStat = lstatSync(join(dest, 'README.md'));
+    assert.notStrictEqual(
+      finalStat.ctime.getTime(),
+      initialStat.ctime.getTime()
+    );
+  }));
+}
 
 // It does not fail if the same directory is copied to dest twice,
-// when dereference is true, and overwrite true.
+// when dereference is true, and force is false (fails silently).
 {
   const src = dirname(require.resolve('../fixtures/copy/kitchen-sink'));
   const dest = nextdir();
   const destFile = join(dest, 'a/b/README2.md');
-  copySync(src, dest, {dereference: true});
+  copySync(src, dest, { dereference: true });
   copy(src, dest, { dereference: true }, common.mustCall((err) => {
     assert.strictEqual(err, null);
     const stat = lstatSync(destFile);
@@ -85,7 +105,31 @@ function nextdir() {
   }));
 }
 
-// It returns error if part of dest path is symlink to src.
+// It returns error if symlink in src points to location in dest.
+{
+  const src = nextdir();
+  mkdirSync(src, { recursive: true });
+  const dest = nextdir();
+  mkdirSync(dest);
+  symlinkSync(dest, join(src, 'link'));
+  copySync(src, dest);
+  copy(src, dest, common.mustCall((err) => {
+    assert.strictEqual(err.code, 'ERR_FS_COPY_TO_SUBDIRECTORY');
+  }));
+}
+
+// It returns error if symlink in dest points to location in src.
+{
+  const src = dirname(require.resolve('../fixtures/copy/kitchen-sink'));
+  const dest = nextdir();
+  mkdirSync(join(dest, 'a'), { recursive: true });
+  symlinkSync(src, join(dest, 'a', 'c'));
+  copy(src, dest, common.mustCall((err) => {
+    assert.strictEqual(err.code, 'ERR_FS_COPY_SYMLINK_TO_SUBDIRECTORY');
+  }));
+}
+
+// It returns error if parent directory of symlink in dest points to src.
 {
   const src = nextdir();
   mkdirSync(join(src, 'a'), { recursive: true });
@@ -196,11 +240,51 @@ if (!isWindows) {
   copySync(src, dest);
   copy(src, dest, {
     dereference: true,
-    overwrite: false,
     errorOnExist: true,
   }, common.mustCall((err) => {
     assert.strictEqual(err.code, 'ERR_FS_COPY_EEXIST');
   }));
+}
+
+// It returns EEXIST error if attempt is made to copy symlink over file.
+{
+  const src = dirname(require.resolve('../fixtures/copy/kitchen-sink'));
+  const dest = nextdir();
+  mkdirSync(join(dest, 'a'), { recursive: true });
+  writeFileSync(join(dest, 'a', 'c'), 'hello', 'utf8');
+  copy(src, dest, common.mustCall((err) => {
+    assert.strictEqual(err.code, 'EEXIST');
+  }));
+}
+
+// It makes file writeable when updating timestamp, if not writeable.
+{
+  const src = nextdir();
+  mkdirSync(src, { recursive: true });
+  const dest = nextdir();
+  mkdirSync(dest, { recursive: true });
+  writeFileSync(join(src, 'foo.txt'), 'foo', { mode: 0o444 });
+  copy(src, dest, { preserveTimestamps: true }, common.mustCall((err) => {
+    assert.strictEqual(err, null);
+    assertDirEquivalent(src, dest);
+    const srcStat = lstatSync(join(src, 'foo.txt'));
+    const destStat = lstatSync(join(dest, 'foo.txt'));
+    assert.strictEqual(srcStat.mtime.getTime(), destStat.mtime.getTime());
+  }));
+}
+
+// Promises implementation of copy.
+
+// It copies a nested folder structure with files, folders, symlinks.
+{
+  const src = dirname(require.resolve('../fixtures/copy/kitchen-sink'));
+  const dest = nextdir();
+  async function testCase() {
+    const p = await fs.promises.copy(src, dest);
+    assert.strictEqual(p, undefined);
+    assertDirEquivalent(src, dest);
+  }
+  testCase();
 }
 
 function assertDirEquivalent(dir1, dir2) {
